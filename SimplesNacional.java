22d2ed486e645c0d0ad489ea9149d49ad736a6c0
VitorRusqui <164022766+Vitor5775@users.noreply.github.com>
2024-06-13 22:32:14 -0300

    Add files via upload

---
+++ SimplesNacional.java
@@ -0,0 +1,18 @@
+
+
+public class SimplesNacional extends Calculo {
+    private double simplesNacional;
+
+    @Override
+    public double obtResultado(double[] _val1) {
+        return 0;
+    }
+
+    @Override
+    public double obtResultado(double receitaBrutaTotal, double aliquotaEfetiva, double valorADeduzir) {
+            this.simplesNacional = ((receitaBrutaTotal * aliquotaEfetiva) - valorADeduzir)/receitaBrutaTotal;
+            resultado = this.simplesNacional;
+
+        return resultado;
+    }
+}
